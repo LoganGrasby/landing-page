8f0cc01acfe80cabf80aa5000ee6f04f5bc7e7e7
Logan Grasby <55932034+LoganGrasby@users.noreply.github.com>
2025-01-24 15:59:12 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,4 +5,6 @@ export NVM_DIR="/root/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
+npm install
+
 npm run dev -- --host 0.0.0.0 --port 5173
