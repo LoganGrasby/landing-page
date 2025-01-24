af6580d1ee012ecb8eb1473c8fc250f4b0678b9f
Logan Grasby <55932034+LoganGrasby@users.noreply.github.com>
2025-01-24 12:14:17 -0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -x
+
+export NVM_DIR="/root/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+npm run dev -- --host 0.0.0.0 --port 5173
